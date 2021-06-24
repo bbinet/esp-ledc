37d7df31473c19b608546a69df57f16ce9578412
Bruno Binet <bruno.binet@gmail.com>
2021-06-24 07:49:55 +0200

    Update example with console to set ledc target duty
    And add cmd_wifi, cmd_system, cmd_nvs stuff

--- main/ledc_example_main.c
+++ main/ledc_example_main.c
@@ -7,80 +7,31 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/ledc.h"
 #include "esp_err.h"
+#include <esp_sleep.h>
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_console.h"
+#include "esp_vfs_dev.h"
+#include "driver/uart.h"
+#include "linenoise/linenoise.h"
+#include "argtable3/argtable3.h"
+#include "cmd_decl.h"
+#include "nvs.h"
+#include "nvs_flash.h"
 
-/*
- * About this example
- *
- * 1. Start with initializing LEDC module:
- *    a. Set the timer of LEDC first, this determines the frequency
- *       and resolution of PWM.
- *    b. Then set the LEDC channel you want to use,
- *       and bind with one of the timers.
- *
- * 2. You need first to install a default fade function,
- *    then you can use fade APIs.
- *
- * 3. You can also set a target duty directly without fading.
- *
- * 4. This example uses GPIO18/19/4/5 as LEDC output,
- *    and it will change the duty repeatedly.
- *
- * 5. GPIO18/19 are from high speed channel group.
- *    GPIO4/5 are from low speed channel group.
- *
- */
-#if CONFIG_IDF_TARGET_ESP32
-#define LEDC_HS_TIMER          LEDC_TIMER_0
-#define LEDC_HS_MODE           LEDC_HIGH_SPEED_MODE
-#define LEDC_HS_CH0_GPIO       (18)
-#define LEDC_HS_CH0_CHANNEL    LEDC_CHANNEL_0
-#define LEDC_HS_CH1_GPIO       (19)
-#define LEDC_HS_CH1_CHANNEL    LEDC_CHANNEL_1
-#endif
 #define LEDC_LS_TIMER          LEDC_TIMER_1
 #define LEDC_LS_MODE           LEDC_LOW_SPEED_MODE
-#if CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32C3
-#define LEDC_LS_CH0_GPIO       (18)
+#define LEDC_LS_CH0_GPIO       (38)
 #define LEDC_LS_CH0_CHANNEL    LEDC_CHANNEL_0
-#define LEDC_LS_CH1_GPIO       (19)
-#define LEDC_LS_CH1_CHANNEL    LEDC_CHANNEL_1
-#endif
-#define LEDC_LS_CH2_GPIO       (4)
-#define LEDC_LS_CH2_CHANNEL    LEDC_CHANNEL_2
-#define LEDC_LS_CH3_GPIO       (5)
-#define LEDC_LS_CH3_CHANNEL    LEDC_CHANNEL_3
+#define LEDC_TEST_DUTY         (1000)
+#define LEDC_TEST_FADE_TIME    (2000)
 
-#define LEDC_TEST_CH_NUM       (4)
-#define LEDC_TEST_DUTY         (4000)
-#define LEDC_TEST_FADE_TIME    (3000)
-
-void app_main(void)
-{
-    int ch;
-
-    /*
-     * Prepare and set configuration of timers
-     * that will be used by LED Controller
-     */
-    ledc_timer_config_t ledc_timer = {
-        .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
-        .freq_hz = 5000,                      // frequency of PWM signal
-        .speed_mode = LEDC_LS_MODE,           // timer mode
-        .timer_num = LEDC_LS_TIMER,            // timer index
-        .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
-    };
-    // Set configuration of timer0 for high speed channels
-    ledc_timer_config(&ledc_timer);
-#ifdef CONFIG_IDF_TARGET_ESP32
-    // Prepare and set configuration of timer1 for low speed channels
-    ledc_timer.speed_mode = LEDC_HS_MODE;
-    ledc_timer.timer_num = LEDC_HS_TIMER;
-    ledc_timer_config(&ledc_timer);
-#endif
+static const char* TAG = "ledc_example";
     /*
      * Prepare individual configuration
      * for each channel of LED Controller
@@ -94,28 +45,7 @@ void app_main(void)
      *         then frequency and bit_num of these channels
      *         will be the same
      */
-    ledc_channel_config_t ledc_channel[LEDC_TEST_CH_NUM] = {
-#if CONFIG_IDF_TARGET_ESP32
-        {
-            .channel    = LEDC_HS_CH0_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_HS_CH0_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .hpoint     = 0,
-            .timer_sel  = LEDC_HS_TIMER,
-            .flags.output_invert = 0
-        },
-        {
-            .channel    = LEDC_HS_CH1_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_HS_CH1_GPIO,
-            .speed_mode = LEDC_HS_MODE,
-            .hpoint     = 0,
-            .timer_sel  = LEDC_HS_TIMER,
-            .flags.output_invert = 0
-        },
-#elif CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32C3
-        {
+static ledc_channel_config_t ledc_channel = {
             .channel    = LEDC_LS_CH0_CHANNEL,
             .duty       = 0,
             .gpio_num   = LEDC_LS_CH0_GPIO,
@@ -123,76 +53,259 @@ void app_main(void)
             .hpoint     = 0,
             .timer_sel  = LEDC_LS_TIMER,
             .flags.output_invert = 0
-        },
-        {
-            .channel    = LEDC_LS_CH1_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_LS_CH1_GPIO,
-            .speed_mode = LEDC_LS_MODE,
-            .hpoint     = 0,
-            .timer_sel  = LEDC_LS_TIMER,
-            .flags.output_invert = 0
-        },
-#endif
-        {
-            .channel    = LEDC_LS_CH2_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_LS_CH2_GPIO,
-            .speed_mode = LEDC_LS_MODE,
-            .hpoint     = 0,
-            .timer_sel  = LEDC_LS_TIMER,
-            .flags.output_invert = 1
-        },
-        {
-            .channel    = LEDC_LS_CH3_CHANNEL,
-            .duty       = 0,
-            .gpio_num   = LEDC_LS_CH3_GPIO,
-            .speed_mode = LEDC_LS_MODE,
-            .hpoint     = 0,
-            .timer_sel  = LEDC_LS_TIMER,
-            .flags.output_invert = 1
-        },
     };
 
-    // Set LED Controller with previously prepared configuration
-    for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
-        ledc_channel_config(&ledc_channel[ch]);
+static struct {
+    struct arg_int *duty;
+    struct arg_end *end;
+} ledc_args;
+
+static int ledc(int argc, char **argv)
+{
+    int nerrors = arg_parse(argc, argv, (void **) &ledc_args);
+    if (nerrors != 0) {
+        arg_print_errors(stderr, ledc_args.end, argv[0]);
+        return 1;
     }
+    if (ledc_args.duty->count) {
+        uint16_t duty = ledc_args.duty->ival[0];
+        /*
+        printf("LEDC fade up to duty = %d\n", duty);
+        ledc_set_fade_with_time(ledc_channel.speed_mode,
+                ledc_channel.channel, duty, LEDC_TEST_FADE_TIME);
+        ledc_fade_start(ledc_channel.speed_mode,
+                ledc_channel.channel, LEDC_FADE_NO_WAIT);
+        vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
+        */
+        printf("LEDC update to duty = %d\n", duty);
+        ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, duty);
+        ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+    }
+
+    return 0;
+}
+
+void register_ledc()
+{
+    ledc_args.duty =
+        arg_int1("d", "duty", "<d>", "target duty");
+    ledc_args.end = arg_end(3);
+
+    const esp_console_cmd_t cmd = {
+        .command = "ledc",
+        .help = "Set new target duty",
+        .hint = NULL,
+        .func = &ledc,
+        .argtable = &ledc_args
+    };
+    ESP_ERROR_CHECK( esp_console_cmd_register(&cmd) );
+}
+
+
+#define PROMPT_STR CONFIG_IDF_TARGET
+
+
+static void initialize_nvs(void)
+{
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+}
+
+static void initialize_console(void)
+{
+    /* Drain stdout before reconfiguring it */
+    fflush(stdout);
+    fsync(fileno(stdout));
+
+    /* Disable buffering on stdin */
+    setvbuf(stdin, NULL, _IONBF, 0);
+
+    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+    esp_vfs_dev_uart_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CR);
+    /* Move the caret to the beginning of the next line on '\n' */
+    esp_vfs_dev_uart_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM, ESP_LINE_ENDINGS_CRLF);
+
+    /* Configure UART. Note that REF_TICK is used so that the baud rate remains
+     * correct while APB frequency is changing in light sleep mode.
+     */
+    const uart_config_t uart_config = {
+            .baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE,
+            .data_bits = UART_DATA_8_BITS,
+            .parity = UART_PARITY_DISABLE,
+            .stop_bits = UART_STOP_BITS_1,
+#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
+        .source_clk = UART_SCLK_REF_TICK,
+#else
+        .source_clk = UART_SCLK_XTAL,
+#endif
+    };
+    /* Install UART driver for interrupt-driven reads and writes */
+    ESP_ERROR_CHECK( uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM,
+            256, 0, 0, NULL, 0) );
+    ESP_ERROR_CHECK( uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config) );
+
+    /* Tell VFS to use UART driver */
+    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+
+    /* Initialize the console */
+    esp_console_config_t console_config = {
+            .max_cmdline_args = 8,
+            .max_cmdline_length = 256,
+#if CONFIG_LOG_COLORS
+            .hint_color = atoi(LOG_COLOR_CYAN)
+#endif
+    };
+    ESP_ERROR_CHECK( esp_console_init(&console_config) );
+
+    /* Configure linenoise line completion library */
+    /* Enable multiline editing. If not set, long commands will scroll within
+     * single line.
+     */
+    linenoiseSetMultiLine(1);
+
+    /* Tell linenoise where to get command completions and hints */
+    linenoiseSetCompletionCallback(&esp_console_get_completion);
+    linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
+
+    /* Set command history size */
+    linenoiseHistorySetMaxLen(100);
+
+    /* Don't return empty lines */
+    linenoiseAllowEmpty(false);
+
+}
+
+void app_main(void)
+{
+    /*
+     * Prepare and set configuration of timers
+     * that will be used by LED Controller
+     */
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
+        .freq_hz = 50,                      // frequency of PWM signal
+        .speed_mode = LEDC_LS_MODE,           // timer mode
+        .timer_num = LEDC_LS_TIMER,            // timer index
+        .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
+    };
+    // Set configuration of timer0 for high speed channels
+    ledc_timer_config(&ledc_timer);
+
+    // Set LED Controller with previously prepared configuration
+    ledc_channel_config(&ledc_channel);
 
     // Initialize fade service.
     ledc_fade_func_install(0);
 
+    int deep_sleep_sec = 5;
+        /*
     while (1) {
         printf("1. LEDC fade up to duty = %d\n", LEDC_TEST_DUTY);
-        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
-            ledc_set_fade_with_time(ledc_channel[ch].speed_mode,
-                    ledc_channel[ch].channel, LEDC_TEST_DUTY, LEDC_TEST_FADE_TIME);
-            ledc_fade_start(ledc_channel[ch].speed_mode,
-                    ledc_channel[ch].channel, LEDC_FADE_NO_WAIT);
-        }
+        ledc_set_fade_with_time(ledc_channel.speed_mode,
+                ledc_channel.channel, LEDC_TEST_DUTY, LEDC_TEST_FADE_TIME);
+        ledc_fade_start(ledc_channel.speed_mode,
+                ledc_channel.channel, LEDC_FADE_NO_WAIT);
         vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
 
         printf("2. LEDC fade down to duty = 0\n");
-        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
-            ledc_set_fade_with_time(ledc_channel[ch].speed_mode,
-                    ledc_channel[ch].channel, 0, LEDC_TEST_FADE_TIME);
-            ledc_fade_start(ledc_channel[ch].speed_mode,
-                    ledc_channel[ch].channel, LEDC_FADE_NO_WAIT);
-        }
+        ledc_set_fade_with_time(ledc_channel.speed_mode,
+                ledc_channel.channel, 0, LEDC_TEST_FADE_TIME);
+        ledc_fade_start(ledc_channel.speed_mode,
+                ledc_channel.channel, LEDC_FADE_NO_WAIT);
+        vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
+
+        ESP_LOGI(TAG, "Entering deep sleep for %d seconds", deep_sleep_sec);
+        //esp_wifi_stop();
+        //adc_power_off();
+        esp_deep_sleep(1000000LL * deep_sleep_sec);
         vTaskDelay(LEDC_TEST_FADE_TIME / portTICK_PERIOD_MS);
 
         printf("3. LEDC set duty = %d without fade\n", LEDC_TEST_DUTY);
-        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
-            ledc_set_duty(ledc_channel[ch].speed_mode, ledc_channel[ch].channel, LEDC_TEST_DUTY);
-            ledc_update_duty(ledc_channel[ch].speed_mode, ledc_channel[ch].channel);
-        }
+        ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, LEDC_TEST_DUTY);
+        ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
         printf("4. LEDC set duty = 0 without fade\n");
-        for (ch = 0; ch < LEDC_TEST_CH_NUM; ch++) {
-            ledc_set_duty(ledc_channel[ch].speed_mode, ledc_channel[ch].channel, 0);
-            ledc_update_duty(ledc_channel[ch].speed_mode, ledc_channel[ch].channel);
-        }
+        ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, 0);
+        ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+        */
+    initialize_nvs();
+
+    ESP_LOGI(TAG, "Command history disabled");
+
+    initialize_console();
+
+    /* Register commands */
+    esp_console_register_help_command();
+    register_system();
+    register_wifi();
+    register_nvs();
+    register_ledc();
+
+    /* Prompt to be printed before each line.
+     * This can be customized, made dynamic, etc.
+     */
+    const char* prompt = LOG_COLOR_I PROMPT_STR "> " LOG_RESET_COLOR;
+
+    printf("\n"
+           "This is an example of ESP-IDF console component.\n"
+           "Type 'help' to get the list of commands.\n"
+           "Use UP/DOWN arrows to navigate through command history.\n"
+           "Press TAB when typing command name to auto-complete.\n"
+           "Press Enter or Ctrl+C will terminate the console environment.\n");
+
+    /* Figure out if the terminal supports escape sequences */
+    int probe_status = linenoiseProbe();
+    if (probe_status) { /* zero indicates success */
+        printf("\n"
+               "Your terminal application does not support escape sequences.\n"
+               "Line editing and history features are disabled.\n"
+               "On Windows, try using Putty instead.\n");
+        linenoiseSetDumbMode(1);
+#if CONFIG_LOG_COLORS
+        /* Since the terminal doesn't support escape sequences,
+         * don't use color codes in the prompt.
+         */
+        prompt = PROMPT_STR "> ";
+#endif //CONFIG_LOG_COLORS
+    }
+
+    /* Main loop */
+    while(true) {
+        /* Get a line using linenoise.
+         * The line is returned when ENTER is pressed.
+         */
+        char* line = linenoise(prompt);
+        if (line == NULL) { /* Break on EOF or error */
+            break;
+        }
+        /* Add the command to the history if not empty*/
+        if (strlen(line) > 0) {
+            linenoiseHistoryAdd(line);
+        }
+
+        /* Try to run the command */
+        int ret;
+        esp_err_t err = esp_console_run(line, &ret);
+        if (err == ESP_ERR_NOT_FOUND) {
+            printf("Unrecognized command\n");
+        } else if (err == ESP_ERR_INVALID_ARG) {
+            // command was empty
+        } else if (err == ESP_OK && ret != ESP_OK) {
+            printf("Command returned non-zero error code: 0x%x (%s)\n", ret, esp_err_to_name(ret));
+        } else if (err != ESP_OK) {
+            printf("Internal error: %s\n", esp_err_to_name(err));
+        }
+        /* linenoise allocates line buffer on the heap, so need to free it */
+        linenoiseFree(line);
+    }
+
+    ESP_LOGE(TAG, "Error or end-of-input, terminating console");
+    esp_console_deinit();
 }
